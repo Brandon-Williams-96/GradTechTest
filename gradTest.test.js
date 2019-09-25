1e859aa61faa02d30805883b005b868b5229bb99
Brandon <96.brandon.williams@gmail.com>
2019-09-25 15:23:19 +0100

    Cleaner

--- gradTest.test.js
+++ gradTest.test.js
@@ -1,37 +1,41 @@
 function createMenuData(data) {
-  var i, j, parent='no', child, n;
-  const parentcheck = [];
-  const test = [];
-  temp = [];
-  const children = [];
-  
-  for (i = 0; i < data.length; i++) {  
-      n=0;
-      temp = []
-      if (data[i].includes('parent') && data[i].includes('/'))
-      {
-        parentcheck.push(parent);
-        parent=data[i].substr(0, data[i].indexOf('/',0));
-        if (parentcheck.includes(parent))
-          {
-            n+=1;
-          }
-        for (j = 0; j < data.length; j++) {
-          if (data[j].includes(parent) && data[j].includes('/'))
-          {
-            child=data[j].slice(data[j].indexOf('/',1)),0;
-            temp.push(child.slice(1));
-          }
-        }
-        if (n==0 && temp.length!=0)
-        {
-          test.push({title: parent, data: temp});
-          n=+1;
-        }
-      
-      }
-    }
-  return test
+	var i, j, parent = 'no',
+		child, n;
+	const parentcheck = [];
+  	const test = [];
+	for (i = 0; i < data.length; i++) {
+		n = 0;
+		temp = []
+		
+		//check to see if parent has a child
+		if (data[i].includes('/')) {
+			parentcheck.push(parent);
+			parent = data[i].substr(0, data[i].indexOf('/', 0));
+			
+			//check to see if parent has already been added
+			if (parentcheck.includes(parent)) {
+				n += 1;
+			}
+			
+			//finds all children of parents
+			for (j = 0; j < data.length; j++) {
+				if (data[j].includes(parent) && data[j].includes('/')) {
+					child = data[j].slice(data[j].indexOf('/', 1)), 0;
+					temp.push(child.slice(1));
+				}
+			}
+			
+			//add parent and their children to list if they haven't already been added
+			if (n == 0 && temp.length != 0) {
+				test.push({
+					title: parent,
+					data: temp
+				});
+				n = +1;
+			}
+		}
+	}
+	return test
 }
 
 
